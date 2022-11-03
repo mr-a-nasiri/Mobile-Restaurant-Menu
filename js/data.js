8f6e504d904f48f7a3a454789acb5d15a3241d21
mr-a-nasiri <mrfinal21@gmail.com>
2022-11-04 00:41:22 +0330

    add UI

--- js/data.js
+++ js/data.js
@@ -1,23 +1,25 @@
-const menuArray = [
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+export const menuArray = [
   {
     name: 'Pizza',
     ingredients: ['pepperoni', 'mushroom', 'mozzarella'],
-    id: 0,
     price: 14,
     emoji: '🍕',
+    uuidv4: uuidv4(),
   },
   {
     name: 'Hamburger',
     ingredients: ['beef', 'cheese', 'lettuce'],
     price: 12,
     emoji: '🍔',
-    id: 1,
+    uuidv4: uuidv4(),
   },
   {
     name: 'Beer',
     ingredients: ['grain, hops, yeast, water'],
     price: 12,
     emoji: '🍺',
-    id: 2,
+    uuidv4: uuidv4(),
   },
 ];
--- js/script.js
+++ js/script.js
@@ -1,9 +1,60 @@
 'use strict';
+
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import { menuArray } from '/js/data.js';
+
+// Values
+const selectedItems = [];
+
+document.addEventListener('click', function (e) {
+  if (e.target.dataset.add) {
+    addItem(e.target.dataset.add);
+  }
+});
+
+function render() {
+  let feedHtml = '';
+
+  menuArray.forEach(item => {
+    feedHtml += `
+      <div class="item">
+        <p class="item-img">${item.emoji}</p>
+        <div class="item-details">
+          <p class="item-name">${item.name}</p>
+          <p class="item-content">${item.ingredients}</p>
+          <p class="item-price">$${item.price}</p>
+        </div>
+        <button class="item-add-btn" data-add="${uuidv4()}"></button>
+      </div>
+    `;
+  });
+
+  document.querySelector('.items').innerHTML = feedHtml;
+}
+render();
+
+function addItem(uuid) {
+  document.getElementById('order-submit').classList.remove('hidden');
+
+  menuArray.forEach(item => {
+    if (item.uuid === uuid) selectedItems.push(item);
+  });
+}
 // Elements & Input
 // Btn Elements
-// Values
+
 // Event listeners
-// loadEventListeners();
+
 // LocalStorage Functions
 // Utility Functions
 // Functions
+
+/* <div class="item">
+  <img src="images/pizza.png" alt="pizza" class="item-img" />
+  <div class="item-details">
+    <p class="item-name">Pizza</p>
+    <p class="item-content">pepperoni,mushroom,mozzarella</p>
+    <p class="item-price">$14</p>
+  </div>
+  <button class="item-add-btn"></button>
+</div> */
